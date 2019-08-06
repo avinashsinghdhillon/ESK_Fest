d13c36e90e4dd2ca51ce21fb6fe21d05fd62f37c
sylvo <49357334+sjonon@users.noreply.github.com>
2019-08-06 02:12:55 -0500

    saved passport.js file

--- passport.js
+++ passport.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
-const User = require('./models/user')
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
 
+//JSON Web Tokens Strategy
 passport.use(new JwtStrategy({
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: process.env.JWT_SECRET,
@@ -23,3 +25,21 @@ passport.use(new JwtStrategy({
         done(error, false);
     }
 }));
+
+//Local Strategy
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, async (email, password, done) => {
+    //find the user given the email
+    const user = await User.findOne({ email });
+    //if not, handle it
+    if (!user) {
+        return done(null, false);
+    }
+    //check if password is correct
+    
+
+    //if not, handle
+
+    //otherwise, return the user
+}))
