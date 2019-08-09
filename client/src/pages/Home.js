13813d641bf18c7cb7b04da52620251f7b4d4a7c
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-09 10:49:58 -0500

    added email link to privacy, added brief about to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,9 +20,11 @@ class Home extends Component {
             <Container fluid>
                 <div>
                 <hr/>
-                <h1>WELCOME TO THE EASTSIDE KINGS MUSIC FESTIVAL, LOCATED IN AUSTIN, TX.</h1>
+                <h1 style={{textAlign:"center"}}>WELCOME TO THE EASTSIDE KINGS MUSIC FESTIVAL</h1><br/>
                 <div id="eventDetails">
-                    <h4>Event Details Here</h4>
+                    <h5>Eastside Kings Fest is a celebration for the great living musicians who have given us some of the best years of music.</h5>
+                    <h5>The Eastside Kings Fest takes place the first week of September putting together great Blues/Gospel/Jazz acts every year for two amazing evenings.</h5>
+                    <h5>This year's show is in East Austin, TX September 14th & 15th, 2019.</h5>
                 </div>
                 <hr/>
                 <h1>SPONSORS</h1>
