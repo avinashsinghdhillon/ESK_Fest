33640c2b691aac3d1d1d72d683fcc905e5e92c26
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-11 12:54:53 -0500

    updated logo ratio, update artist card and itinerary

--- client/src/pages/Itinerary.js
+++ client/src/pages/Itinerary.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import { Container } from '../components/Grid';
 import ArtistCard from '../components/ArtistCard';
+import API from '../utils/API';
 
 class Itinerary extends Component {
+
+    state = {
+        userName: "",
+        // artists: [],
+        events: []
+    }
+
+    componentDidMount() {
+        this.loadEvents();
+    }
+
+    loadEvents = () => {
+        API.getEvents()
+        .then(res => {
+            console.log("loading saved events")
+            console.log(res.data)
+            this.setState({ events: res.data })
+        })
+    }
+
     render() {
         return (
             <Container>
             <h1>Welcome back, USER NAME.</h1>
             <h3>Here are the events you were interested in.</h3>
             <div>
-                <ArtistCard />
-                <ArtistCard />
-                <ArtistCard />
-                <ArtistCard />
-                <ArtistCard />
-
-            {/* {this.state.events.map((events, index) => {
+            {this.state.events.map((events, index) => {
                 return(<ArtistCard
                     key={index}
                     id={events.id}
                     src={events.artistPicture}
                     name={events.artistName}
                     />)
-            })} */}
+            })}
             </div>
         </Container>
         )
