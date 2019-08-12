14dccea8d38dc818f982278f304c72f84e508359
Brittany Taylor <hopkins.britt@gmail.com>
2019-08-12 10:48:38 -0500

    added top padding to Contact page and routed footer to it

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
             <ul>
                 <li><a href="/sponsors" onClick={() => props.handlePageChange("Sponsors")}>Sponsors</a></li>
                 <li><a href="/privacy" onClick={() => props.handlePageChange("Privacy")}>Privacy</a></li>
-                <li><a href="/contactus" onClick={() => props.handlePageChange("Contact")}>Contact Us</a></li>
+                <li><a href="/contact" onClick={() => props.handlePageChange("Contact")}>Contact Us</a></li>
             </ul>
             <p>©2019 Endemic Design</p>
             <p>This project is supported in part by the Cultural Arts Division of the City of Austin Economic Development Department.</p>
