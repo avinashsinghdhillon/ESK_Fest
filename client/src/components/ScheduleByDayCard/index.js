b9dc9edd0c63c0fd6e35e08117494bf7bb0f6623
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-13 14:31:51 -0500

    updating schedulebyday, adding itinerary add btn functionality

--- client/src/components/ScheduleByDayCard/index.js
+++ client/src/components/ScheduleByDayCard/index.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import "./style.css";
 
 class ScheduleByDayCard extends Component {
+
+  state = {
+    clicked: false,
+    // saved: "",
+  }
+
+  itineraryClick = (bool) => {
+    if (this.state.clicked === false) {
+      this.setState({
+        clicked: true,
+        // saved: true
+      })
+    } else {
+      this.setState({
+        clicked: false,
+        // saved: false
+      })
+    }
+  }
 
   render(props) {
     return (
       <div>
         <div className="card scheduleByDay">
-          {/* <h5> */}
-              <h4 style={{textAlign: "center"}}><span className="dateTag">Date: {this.props.startDate}</span></h4><hr/>
-            <div className="eventText">
-              <h4>Times: {this.props.startTime} - {this.props.endTime}</h4>
-              <h4>Artists: {this.props.artists}</h4>
-              <h4>Location: {this.props.location}</h4>
+        <button className="saved" onClick={() => this.itineraryClick()}>
+            {/* IF EVENT IS SAVED, SHOW THIS */}
+            {/* <FontAwesomeIcon icon={["fas", "bookmark"]} /> */}
+            {/* IF EVENT IS NOT SAVED, SHOW THIS */}
+              {
+                this.state.clicked 
+                  ? <span><FontAwesomeIcon icon={['fas', 'bookmark']} /> Saved to Itinerary</span>
+                  : <span><FontAwesomeIcon icon={['far', 'bookmark']} /> Save to Itinerary</span>
+              }
+        </button>
+              <h4 style={{textAlign:"center"}}><span className="dateTag">Date: {this.props.startDate}</span></h4><hr/>
+            <div className="box">
+              <div className="eventText">
+                <h4>Times:</h4> <h3>{this.props.startTime} - {this.props.endTime}</h3>
+                <h4>Artists:</h4> <h3>{this.props.artists}</h3>
+                <h4>Location:</h4> <h3>{this.props.location}</h3>
+              </div>
+              <div className="map">
+                <img src={this.props.map} alt="venue location"/>
+              </div>
             </div>
-            <div className="map">
-              <img src={this.props.map} alt="venue location"/>
-            </div>
-          {/* </h5> */}
-          <button className="button" style={{margin: "auto", fontSize:"20px"}}>More Info</button>
+          {/* <button className="button" style={{margin: "auto", fontSize:"20px"}}>More Info</button> */}
         </div>
       </div>
     )
