d4bae886cc94580fbdb5d5ac17cb330d8fe09bc7
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-13 18:40:15 -0500

    init itinerary comp map

--- client/src/components/ScheduleByDayCard/index.js
+++ client/src/components/ScheduleByDayCard/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import "./style.css";
+import "../../pages/ScheduleDay.css";
 
 class ScheduleByDayCard extends Component {
 
--- client/src/pages/Itinerary.js
+++ client/src/pages/Itinerary.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import { Container } from '../components/Grid';
-import ArtistCard from '../components/ArtistCard';
+import ScheduleByDayCard from '../components/ScheduleByDayCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import API from '../utils/API';
 
-import * as actions from '../actions';
+// import * as actions from '../actions';
 
 class Itinerary extends Component {
-    async componentDidMount(){
-        this.props.getItinerary(this.props.auth.id)
+
+    state = {
+        user: "",
+        events: [],
+        artists: [],
+    }
+
+    componentDidMount() {
+        this.getItinerary();
+    }
+
+    getItinerary = event => {
+        API.getUserItineraries
+        .then((req,res) => {
+            console.log(res.data)
+            this.setState({ events: res.data, user: req.user })
+        })
     }
 
     render() {
         return (
             <Container>
-            <h1>Welcome back, USER NAME.</h1>
+            <h1 style={{textAlign:"center", fontFamily:"Shrikhand, cursive"}}>Welcome back, USER FIRST NAME!</h1>
             <h3>Here are the events you were interested in.</h3>
             <div>
             {this.state.events.map((events, index) => {
-                return(<ArtistCard
+                return(
+                <ScheduleByDayCard
                     key={index}
-                    id={events.id}
-                    src={events.artistPicture}
-                    name={events.artistName}
-                    />)
+                    id={user.id}
+                    events={events.id}
+                    date={events.startDate}
+                    startTime={events.startTime}
+                    endTime={events.endTime}
+                    location={events.locationId}
+                    artists={events.artists}
+                    map={events.map}
+                />)
             })}
             </div>
         </Container>
@@ -31,13 +53,4 @@ class Itinerary extends Component {
     }
 }
 
-function mapStateToProps(state) {
-    console.log('state', state)
-    return {
-      secret: state.itin.secret,
-      dashboard: state.itin,
-      auth: state.auth
-    }
-  }
-
-export default connect(mapStateToProps, actions)(Itinerary);
+export default Itinerary;
